22167c5b0a6f90c82ec2b4cab378534c6ff86b64
viponcio <vitoria.poncio@gmail.com>
2021-09-22 12:12:19 -0300

    virtual

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
-from app.forms import ProdutosForm
+from app.forms import CompanyForm, ProdutosForm
 from app.models import Produtos
+from django.http import HttpResponse
+
 
 # Create your views here.
 def home (request):
@@ -17,6 +19,9 @@ def createproduct(request):
     form = ProdutosForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('products-form')
+        return redirect('home')
 
-
+def companyform (request):
+    data = {} #dicionario
+    data['companyform'] = CompanyForm()
+    return render(request, 'company-form.html', data)
